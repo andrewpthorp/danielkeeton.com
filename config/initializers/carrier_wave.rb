5f8d42cfb966c0ee2b284455ed3730b27a8b069c
Andrew Thorp <andrewpthorp@gmail.com>
2013-05-10 09:51:53 -0500

    store with fog in production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],
