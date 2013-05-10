9fd698ad6318b5d9686544cb79301fd912f4e235
Andrew Thorp <andrewpthorp@gmail.com>
2013-05-10 10:25:41 -0500

    listings_helper

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,7 +1,18 @@
 module ListingsHelper
 
   def pretty_status(listing)
-    return "" if listing.available?
+    case listing.status
+    when 'for_sale'
+      "(For Sale)"
+    when 'for_rent'
+      "(For Rent)"
+    when 'under_contract'
+      "(Under Contract)"
+    when 'sold'
+      "(Sold)"
+    when 'cancelled'
+      "(Cancelled)"
+    end
   end
 
 end
