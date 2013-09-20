a26fd40f700b842e1213fa50d014563e3ab31355
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-20 10:39:28 -0500

    mailer spec

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -1,5 +1,49 @@
 require "spec_helper"
 
 describe Mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.home_worth_email' do
+    before do
+      @mail = Mailer.home_worth_email({name: 'Andrew'})
+    end
+
+    it 'should render the subject' do
+      expect(@mail.subject).to eq("Website - What's My Home Worth")
+    end
+
+    it 'should send the email to the right person' do
+      expect(@mail.to).to eq(['danielspencerkeeton@gmail.com'])
+    end
+
+    it 'should set the sender to the right email' do
+      expect(@mail.from).to eq(['danielspencerkeeton+website@gmail.com'])
+    end
+
+    it 'should assign @data' do
+      expect(@mail.body.encoded).to match('Andrew')
+    end
+  end
+
+  describe '.contact_email' do
+    before do
+      @mail = Mailer.contact_email({name: 'Daniel'})
+    end
+
+    it 'should render the subject' do
+      expect(@mail.subject).to eq('Website - Contact Form')
+    end
+
+    it 'should send the email to the right person' do
+      expect(@mail.to).to eq(['danielspencerkeeton@gmail.com'])
+    end
+
+    it 'should set the sender to the right email' do
+      expect(@mail.from).to eq(['danielspencerkeeton+website@gmail.com'])
+    end
+
+    it 'should assign @data' do
+      expect(@mail.body.encoded).to match('Daniel')
+    end
+  end
+
 end
