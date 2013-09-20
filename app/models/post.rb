e5799919d97d7deff1ac87187cd22950cec73b09
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-20 10:57:59 -0500

    refactored post_spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,15 @@
+# Public: The Post model handles the blog of the website.
 class Post < ActiveRecord::Base
+
+  # Internal: Allow mass-assignment.
   attr_accessible :body, :published, :title
 
-  validates :title, presence: true
-  validates :body, presence: true
+  # Internal: Validate presence pf specific attributes.
+  validates :title, :body, presence: true
 
+  # Public: Get all Posts that are published.
+  #
+  # Returns an ActiveRecord::Relation.
   scope :published, where(published: true)
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,20 +2,23 @@ require 'spec_helper'
 
 describe Post do
 
-  describe '#validations' do
+  describe '.mass-assignment' do
+    it { should allow_mass_assignment_of(:title) }
+    it { should allow_mass_assignment_of(:body) }
+    it { should allow_mass_assignment_of(:published) }
+  end
+
+  describe '.validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
 
-  describe '#scopes' do
-    before do
-      @published = FactoryGirl.create(:published_post)
-      @post = FactoryGirl.create(:post)
-    end
+  describe '.scopes' do
+    describe '.published' do
+      let (:hash) { Post.published.where_values_hash }
 
-    describe '#published' do
-      it 'should only return published posts' do
-        Post.published.should eq([@published])
+      it 'should have the right conditions' do
+        expect(hash).to eq('published' => true)
       end
     end
   end
