b4cff304b59e0092a229d55ce8b5fb5efff1a057
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-20 10:55:20 -0500

    refactored listing_spec

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,33 +1,85 @@
+# Public: The Listing class handles the storage of real estate listings in the
+# website.
 class Listing < ActiveRecord::Base
+
+  # Internal: Valid states for a Listing.
+  VALID_STATES = %w(for_sale for_rent under_contract sold cancelled)
+
+  # Internal: Allow mass-assignment.
   attr_accessible :title, :description, :status, :price, :featured, :link,
                   :address_line_1, :address_line_2, :city, :state, :zip,
                   :bedrooms, :bathrooms, :year_built, :square_footage,
                   :lot_size, :image
 
-  VALID_STATES = %w(for_sale for_rent under_contract sold cancelled)
+  # Internal: Use CarrierWave to handle Images for the Listings.
+  mount_uploader :image, ListingUploader
 
-  validates :title, presence: true
-  validates :description, presence: true
+  # Internal: Validate presence of specific attributes.
+  validates :title, :description, :price, presence: true
+
+  # Internal: Validate presence and valid state of :status.
   validates :status, presence: true, inclusion: { in: Listing::VALID_STATES }
-  validates :price, presence: true
-  validates :bathrooms, presence: true, numericality: { greater_than_or_equal_to: 0.5 }
-  validates :bedrooms, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :year_built, numericality: { greater_than_or_equal_to: 1900 }, allow_nil: true
-  validates :square_footage, numericality: { greater_than_or_equal_to: 1000 }, allow_nil: true
-  validates :lot_size, numericality: { greater_than_or_equal_to: 1000 }, allow_nil: true
 
+  # Internal: Validate presence and correct numericality of :bathrooms.
+  validates :bathrooms, presence: true,
+                        numericality: { greater_than_or_equal_to: 0.5 }
+
+  # Internal: Validate presence and correct numericality of :bedrooms.
+  validates :bedrooms, presence: true,
+                        numericality: { only_integer: true, greater_than: 0 }
+
+  # Internal: Validate numericality of :year_built, allow it to be nil.
+  validates :year_built, numericality: { greater_than_or_equal_to: 1900 },
+                          allow_nil: true
+
+  # Internal: Validate numericality of :square_footage, allow it to be nil.
+  validates :square_footage, numericality: { greater_than_or_equal_to: 1000 },
+                              allow_nil: true
+
+  # Internal: Validate numericality of :lot_size, allow it to be nil.
+  validates :lot_size, numericality: { greater_than_or_equal_to: 1000 },
+                        allow_nil: true
+
+  # Internal: Ensure that only one Listing is featured.
   before_save :ensure_single_featured
 
+  # Public: Get all Listings where featured is set to true.
+  #
+  # Returns an ActiveRecord::Relation.
   scope :featured, where(featured: true)
+
+  # Public: Get all Listings where status is set to 'for_sale.'
+  #
+  # Returns an ActiveRecord::Relation.
   scope :for_sale, where(status: 'for_sale')
+
+  # Public: Get all Listings where status is set to 'for_rent.'
+  #
+  # Returns an ActiveRecord::Relation.
   scope :for_rent, where(status: 'for_rent')
+
+  # Public: Get all Listings where status is set to 'under_contract.'
+  #
+  # Returns an ActiveRecord::Relation.
   scope :under_contract, where(status: 'under_contract')
+
+  # Public: Get all Listings where status is set to 'sold.'
+  #
+  # Returns an ActiveRecord::Relation.
   scope :sold, where(status: 'sold')
 
-  mount_uploader :image, ListingUploader
+  # Public: Get all Listings where status is set to 'cancelled.'
+  #
+  # Returns an ActiveRecord::Relation.
+  scope :cancelled, where(status: 'cancelled')
 
 private
 
+  # Internal: Ensure that only one Listing is featured. If the current Listing
+  # is not featured, this is basically a noop. If the current Listing is
+  # featured, we update all other Listings to not be featured.
+  #
+  # Returns nothing.
   def ensure_single_featured
     Listing.all.delete_if { |l| l == self }.each do |listing|
       listing.update_column(:featured, false)
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -2,39 +2,54 @@ require 'spec_helper'
 
 describe Listing do
 
-  describe "#validations" do
+  describe '.mass-assignment' do
+    it { should allow_mass_assignment_of(:title) }
+    it { should allow_mass_assignment_of(:description) }
+    it { should allow_mass_assignment_of(:status) }
+    it { should allow_mass_assignment_of(:price) }
+    it { should allow_mass_assignment_of(:featured) }
+    it { should allow_mass_assignment_of(:link) }
+    it { should allow_mass_assignment_of(:address_line_1) }
+    it { should allow_mass_assignment_of(:address_line_2) }
+    it { should allow_mass_assignment_of(:city) }
+    it { should allow_mass_assignment_of(:state) }
+    it { should allow_mass_assignment_of(:zip) }
+    it { should allow_mass_assignment_of(:bedrooms) }
+    it { should allow_mass_assignment_of(:bathrooms) }
+    it { should allow_mass_assignment_of(:year_built) }
+    it { should allow_mass_assignment_of(:square_footage) }
+    it { should allow_mass_assignment_of(:lot_size) }
+    it { should allow_mass_assignment_of(:image) }
+  end
+
+  describe '.validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:status) }
-    it { should ensure_inclusion_of(:status).in_array(Listing::VALID_STATES) }
     it { should validate_presence_of(:price) }
-
     it { should validate_presence_of(:bathrooms) }
+    it { should validate_presence_of(:bedrooms) }
+    it { should ensure_inclusion_of(:status).in_array(Listing::VALID_STATES) }
     it { should validate_numericality_of(:bathrooms) }
     it { should_not allow_value(0).for(:bathrooms) }
-
-    it { should validate_presence_of(:bedrooms) }
     it { should validate_numericality_of(:bedrooms).only_integer }
     it { should_not allow_value(0).for(:bedrooms) }
-
     it { should validate_numericality_of(:year_built) }
     it { should allow_value(1900).for(:year_built) }
     it { should allow_value(nil).for(:year_built) }
     it { should_not allow_value(1899).for(:year_built) }
-
     it { should validate_numericality_of(:square_footage) }
     it { should allow_value(nil).for(:square_footage) }
     it { should allow_value(1000).for(:square_footage) }
     it { should_not allow_value(999).for(:square_footage) }
-
     it { should validate_numericality_of(:lot_size) }
     it { should allow_value(nil).for(:lot_size) }
     it { should allow_value(1000).for(:lot_size) }
     it { should_not allow_value(999).for(:lot_size) }
   end
 
-  describe '#callbacks' do
-    describe '#ensure_single_featured' do
+  describe '.callbacks' do
+    describe '.ensure_single_featured' do
       it 'should not unset if the current Listing is not featured' do
         @featured = FactoryGirl.create(:featured_listing)
         @unfeatured = FactoryGirl.create(:listing)
@@ -60,48 +75,23 @@ describe Listing do
     end
   end
 
-  describe '#scopes' do
-    before do
-      @featured = FactoryGirl.create(:featured_listing, status: 'for_sale')
-      @for_rent = FactoryGirl.create(:listing, status: 'for_rent')
-      @under_contract = FactoryGirl.create(:listing, status: 'under_contract')
-      @sold = FactoryGirl.create(:listing, status: 'sold')
-    end
+  describe '.scopes' do
+    describe '.featured' do
+      let (:hash) { Listing.featured.where_values_hash }
 
-    describe '#featured' do
       it 'should only return featured Listings' do
-        Listing.featured.should eq([@featured])
+        expect(hash).to eq({'featured' => true})
       end
     end
 
-    describe '#for_sale' do
-      it 'should only return Listings with a status of for_sale' do
-        Listing.for_sale.should eq([@featured])
+    Listing::VALID_STATES.each do |status|
+      describe ".#{status}" do
+        let(:hash) { Listing.send(status.to_sym).where_values_hash }
+
+        it "should only return listings with a status of #{status}" do
+          expect(hash).to eq('status' => status)
+        end
       end
     end
-
-    describe '#for_rent' do
-      it 'should only return Listings with a status of for_rent' do
-        Listing.for_rent.should eq([@for_rent])
-      end
-    end
-
-    describe '#under_contract' do
-      it 'should only return Listings with a status of under_contract' do
-        Listing.under_contract.should eq([@under_contract])
-      end
-    end
-
-    describe '#sold' do
-      it 'should only return Listings with a status of sold' do
-        Listing.sold.should eq([@sold])
-      end
-    end
-  end
-
-  describe '#methods' do
-  end
-
-  describe '.methods' do
   end
 end
