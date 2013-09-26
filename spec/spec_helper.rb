ed4b86551c9c711685bebf00cbe81259f10c10f2
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-26 12:05:31 -0500

    added Devise helpers to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,4 +37,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include DeviseMacros
+  config.include Devise::TestHelpers, :type => :controller
 end
