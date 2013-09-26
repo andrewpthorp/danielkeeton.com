26904f6b69fc24bbf6ff85f0b57d205ce48dafd4
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-26 14:14:25 -0500

    updates to admin sign in, styles, etc

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,10 @@ module DanielkeetonCom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Set devise layout
+    config.to_prepare do
+      Devise::SessionsController.layout 'minimal'
+    end
   end
 end
