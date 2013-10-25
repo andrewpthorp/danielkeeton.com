0083d5eb9327f8777918d980941e8312d592f01e
Andrew Thorp <andrewpthorp@gmail.com>
2013-10-24 21:09:58 -0500

    updated seeds

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :post do
     title { Faker::Lorem.sentence(8) }
-    body { "<p>#{Faker::Lorem.paragraphs(3).join('</p><p>')}</p>" }
+    body { Faker::Lorem.paragraphs(3).join("\n\n") }
     published false
 
     factory :published_post do
