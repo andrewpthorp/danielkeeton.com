02c99e3861c34fff21764ba41e55c759922556c1
Andrew Thorp <andrewpthorp@gmail.com>
2013-10-25 21:15:23 -0500

    refactor\!

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,16 +7,7 @@ class ListingsController < ApplicationController
       render_404 and return
     end
 
-    case @status
-    when 'for_sale'
-      @listings = Listing.for_sale
-    when 'for_rent'
-      @listings = Listing.for_rent
-    when 'under_contract'
-      @listings = Listing.under_contract
-    when 'sold'
-      @listings = Listing.sold
-    end
+    @listings = Listing.send(@status)
   end
 
   def show
