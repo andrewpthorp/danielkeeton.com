5a19ab54185c3f5d2b336fdc1c8f199a17e5e7e2
Thaissa Mendes <thaissa.mendes@gmail.com>
2015-03-20 17:26:34 -0300

    Vendors (#7630)

--- app/uploaders/vendor_uploader.rb
+++ app/uploaders/vendor_uploader.rb
@@ -37,11 +37,11 @@ class VendorUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :scale => [nil, 300]
+  end
 
-  process :resize_to_limit => [nil, 300]
+  # process :resize_to_limit => [nil, 300]
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
